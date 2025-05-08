c34e5dfa3ceb3364d955def6386aa807cbafb465
Maksym Krasnov <maksimkrasnov747@gmail.com>
2025-05-08 20:27:52 +0200

    Update movie search functionality and enhance UI with CSS styles

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const filmFragment = document.createDocumentFragment();
 const moviesContainer = document.getElementById("movies-container");
 const topicBtns = document.querySelectorAll(".suggested");
+const searchInput = document.getElementById("search-input");
 let moviesData = [];
+let filteredMoviesData = [];
 function renderList(data) {
+	moviesContainer.innerHTML = '';
+
+	if (filteredMoviesData.length === 0) {
+		const noMatch = document.createElement('p');
+		noMatch.textContent = 'No result for your search';
+		moviesContainer.append(noMatch);
+	}
+
   data.forEach((film) => {
     const filmBox = document.createElement("div");
     const filmTitle = document.createElement("h4");
     const filmYear = document.createElement("p");
     const filmPoster = document.createElement("img");
+
+
 
     filmTitle.textContent = film.Title;
     filmYear.textContent = film.Year;
@@ -15,18 +27,21 @@ function renderList(data) {
 
     filmBox.append(filmTitle, filmYear, filmPoster);
     filmFragment.append(filmBox);
+
   });
-	
+
   moviesContainer.append(filmFragment);
 }
 
-function fetchData(type = `man`) {
+function fetchData(type = `star`) {
   fetch(`https://www.omdbapi.com/?apikey=564727fa&s=${type}`)
     .then((response) => response.json())
     .then((data) => {
       if (data.Response === "True") {
+				searchInput.value = '';
         moviesData = data.Search;
-        renderList(moviesData);
+        filteredMoviesData = data.Search;
+        renderList(filteredMoviesData);
       } else {
         moviesContainer.innerHTML = "No results found";
       }
@@ -44,3 +59,16 @@ topicBtns.forEach((button) => {
     fetchData(topic);
   });
 });
+
+searchInput.addEventListener("input", (e) => {
+  if (e.target.value.trim() !== "") {
+    filteredMoviesData = moviesData.filter((film) => {
+      return film.Title.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    renderList(filteredMoviesData);
+	} else {
+    renderList(moviesData);
+  }
+});
+
+fetchData()
